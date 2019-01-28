9fe9524bfa8aa57149f7a8dc206ff98423b14f8e
samson2 <nobindog1@gmail.com>
2019-01-28 17:17:00 -0600

    Add files via upload

---
+++ prime_number_finder/prime_number_finder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace prime_number_finder
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string and I will find prime numbers");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count;
+
+            for(count = 0; count <= number; count ++)
+            {
+                if (count % 2 != 0 && count % 3 != 0 && count % 5 != 0 && count % 7 != 0)
+                    Console.WriteLine(count);
+            }
+
+
+        }
+    }
+}
